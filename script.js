77e7e5fb5819353f07a0c00ecde0f3d51e37fbe2
Sayyeda Afshan <syed1995.sa@gmail.com>
2024-01-10 20:41:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const form=document.getElementById("form");
+let exicute=(event)=>
+	{
+		event.preventDefault();
+		alert(event.target.FirstName.value);
+		alert(event.target.LastName.value);
+		alert(event.target.LastName.value);
+		alert(event.target.PhoneNumber.value);
+		alert(event.target.EmailID.value);
+		
+	}
+// const exicute=(event)=> {
+// return	alert("clicked");
+	
+// }
+form.addEventListener("submit" ,exicute);
+
+// alert();
